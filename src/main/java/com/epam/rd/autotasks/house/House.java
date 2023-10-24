607266990867e08733c33b0377614a7cef2202a4
Work <vanda70601s@mail.ru>
2023-10-24 23:31:44 +0300

    Commit

--- src/main/java/com/epam/rd/autotasks/house/House.java
+++ src/main/java/com/epam/rd/autotasks/house/House.java
@@ -1,13 +1,15 @@
 package com.epam.rd.autotasks.house;
 
+import com.epam.rd.autotasks.residents.dogs.Dog;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class House {
+public class House <T> {
 
-    private final List residents = new ArrayList();
+    private final List <T> residents = new ArrayList ();
 
-    public void enter(Object resident) {
+    public void enter(T resident) {
         residents.add(resident);
     }
 
@@ -15,7 +17,7 @@ public class House {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("There are following residents in the house:\n");
-        for (Object resident : residents) {
+        for (T resident : residents) {
             builder.append(resident.toString()).append("\n");
         }
         return builder.toString();
